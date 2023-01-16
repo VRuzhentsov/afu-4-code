d68cf6b725c67a232000b726c93ad0b5149956fb
Vitalii Ruzhentsov <v.ruzhentsov@gmail.com>
2023-01-16 17:25:20 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AFU-4-code',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
